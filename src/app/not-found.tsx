7ed1759d07d9ff5f5ce31986e4ca57fa06cbfe14
Soheil Nikroo <soheiln1234@gmail.com>
2024-02-15 21:21:26 +0330

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import notFoundAnimationData from "../../public/animations/404.json";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-[80vh] flex-col items-center justify-center">
+      <Player src={notFoundAnimationData} autoplay loop speed={1} />
+      <p className="mx-auto text-3xl font-medium text-slate-200">
+        Whoops, we couldn&apos;d that page.
+      </p>
+      <Link
+        href="/"
+        className="border-3 mt-8 rounded border-slate-900 bg-slate-50 px-8 py-4 text-2xl font-medium text-slate-900"
+      >
+        Back Home
+      </Link>
+    </div>
+  );
+}
